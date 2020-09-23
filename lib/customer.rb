98c25583e9085818cbca605ebfe8f7da9719cb01
Iris Lux <iris.lux0@gmail.com>
2020-09-23 10:44:04 -0700

    #self.all

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,24 @@
+require 'csv'
+
+#require_relative "../data/customers.csv"
+
 
 class Customer
+
+  def self.all
+    headers = [:id, :email, :street, :city, :state, :zip]
+    customers = CSV.parse(File.read("./data/customers.csv"), headers: headers).map do |row|
+      # address = Hash.new
+      # headers.pop(4).each{|header| address[header] = row[header]}
+      Customer.new(row[:id].to_i, row[:email], {street: row[:street], city: row[:city], state: row[:state], zip: row[:zip]})
+    end
+    return customers
+  end
+
+  def self.find(id)
+    return self.all
+  end
+
   attr_reader :id
   attr_accessor :email, :address
 
