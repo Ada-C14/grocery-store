3c9a4cc115bef0b2c9247964f9ecb1d69f89739e
Pauline Chane <pauline.chane@gmail.com>
2020-09-24 00:07:39 -0700

    Wave 2 updates implemented for customer.rb Customer class. All minitests passed.

--- lib/customer.rb
+++ lib/customer.rb
@@ -3,8 +3,16 @@
 # Grocery Store - customer.rb
 # 9/28/2020
 
+# frozen_string_literal: true
+
+# customer data and imports
+require 'csv'
+
 # Class object spec file for Customer class.
 class Customer
+  # constants
+  ADDRESS_TEMPLATE = %i[street city state zip].freeze
+
   # generic getters/setters
   attr_accessor :email, :address
   attr_reader :id
@@ -15,4 +23,25 @@ class Customer
     @email = email
     @address = address
   end
+
+  # class methods
+
+  # based on an external .csv file containing customer data,
+  # returns collection (in this case, array) of customer instances
+  def self.all
+    customers_raw = CSV.read('data/customers.csv')
+
+    all_customers = customers_raw.map do |customer|
+      Customer.new(customer[0].to_i, customer[1], { street: customer[2], city: customer[3], state: customer[4], zip: customer[5] })
+    end
+
+    return all_customers
+  end
+
+  # returns an instance of Customer matched by id value entered as parameter in self.all
+  # returns nil if id not found
+  def self.find(id)
+    return self.all.find{|customer| customer.id == id}
+  end
 end
+
