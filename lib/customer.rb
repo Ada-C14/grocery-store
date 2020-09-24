72892f2bd72f2f3d0deae4aef75b312d257c33cf
Kareha Agesa <kmaagesa@gmail.com>
2020-09-24 13:13:20 -0700

    a Customer.self method that works with rake

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -8,4 +10,26 @@ class Customer
     @address = delivery_address
   end
 
+  def self.all
+
+    customer_list = []
+    # for rake
+    cust = CSV.read("data/customers.csv")
+
+    # cust = CSV.read("../data/customers.csv")
+    cust.each do |customer|
+      id_number = customer[0].to_i
+      email_address = customer[1]
+      delivery_address = {
+          street: customer[2],
+          city: customer[3],
+          state: customer[4],
+          zip: customer[5]
+      }
+      customer_list.push(Customer.new(id_number, email_address, delivery_address))
+    end
+    return customer_list
+  end
 end
+
+# puts Customer.all
