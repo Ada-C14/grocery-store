6d1abcacc709659175a70846891253ea6aa68fac
Jasmine <jasylop@gmail.com>
2020-09-23 21:33:37 -0700

    added total method

--- lib/order.rb
+++ lib/order.rb
@@ -4,12 +4,18 @@ class Order
   def initialize(id, products_and_prices, customer, fulfillment_status = :pending)
     statuses = %i[pending paid processing shipped complete]
     if !statuses.include?(@fulfillment_status)
-      raise ArgumentError, "#{@fulfillment_status} is an INVALID STatus"
+      raise ArgumentError, "#{@fulfillment_status} is an INVALID status."
     end
     @id = id
     @products_and_prices = products_and_prices
     @customer = customer
     @fulfillment_status = fulfillment_status
+  end
 
+  def total
+    sum = @products_and_prices.values.sum
+    tax = sum * 0.75
+    total = (sum + tax).round(2)
+    return total
   end
 end
