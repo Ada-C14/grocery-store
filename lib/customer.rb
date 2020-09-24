565af6b84c53612a2ec7a0918857bafc13137c5b
Jasmine <jasylop@gmail.com>
2020-09-24 09:02:03 -0700

    updated variable names to match tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,11 @@
 class Customer
 
   attr_reader :id
-  attr_accessor :email, :delivery_address
+  attr_accessor :email, :address
 
-  def initialize(id, email, delivery_address)
+  def initialize(id, email, address)
     @id = id
     @email = email
-    @delivery_address
+    @address = address
   end
 end
