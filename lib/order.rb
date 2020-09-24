9b956ea052cff83ba8ba9a0dedbbad3b686b776e
Jasmine <jasylop@gmail.com>
2020-09-23 21:15:38 -0700

    created Order class and created instance avariables, added arugment error for fulfillment_status

---
+++ lib/order.rb
@@ -0,0 +1,15 @@
+class Order
+  attr_reader :id
+
+  def initialize(id, products_and_prices, customer, fulfillment_status = :pending)
+    statuses = %i[pending paid processing shipped complete]
+    if !statuses.include?(@fulfillment_status)
+      raise ArgumentError, "#{@fulfillment_status} is an INVALID STatus"
+    end
+    @id = id
+    @products_and_prices = products_and_prices
+    @customer = customer
+    @fulfillment_status = fulfillment_status
+
+  end
+end
