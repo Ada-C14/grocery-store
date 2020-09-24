6ba330ddb561ab2a0a033d04229901b941a38e9b
Ren Carothers <rencarothers@gmail.com>
2020-09-23 23:37:01 -0700

    wave 2 first attempt, added methods but not passing all tests q

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
 
   def initialize(id, email, address)
@@ -6,7 +8,44 @@ class Customer
   @address = address
   end
 
-  attr_reader :id, :address
-  attr_accessor :name, :email
+  attr_reader :id
+  attr_accessor :email, :address
 
+  def self.all
+
+    customer_array = [] # will become array of customer instances
+
+    CSV.read('data/customers.csv').each do |row_entry| # FIX PATH I keep getting minitest glitch no file errors so i put the file in each folder lol!!!!
+      # first, define which entries should be contained in the address
+      address = {
+          street: row_entry[2],
+          city: row_entry[3],
+          state: row_entry[4],
+          zip: row_entry[5]
+      }
+      # next, create a new customer instance for each row, adding in the id and email data, along with our newly created address variable
+      new_customer = Customer.new(row_entry[0].to_i, row_entry[1], address) # (Q: why can't I call self.new here?)
+      # push this to the array
+      customer_array << new_customer
+    end
+
+    return customer_array
+
+    # return array of instances
+    # to be invoked by self.find
+  end
+
+  def self.find(id_handed)
+    # search thru self.all for instance with id handed, return customer instance
+
+    self.all.find do |customer_instance|
+      if customer_instance.id == id_handed
+        return customer_instance
+      end
+    end
+
+    # if id matches customer, return customer
+    # not parsing csv, parse arr!
+    # return customer
+  end
 end
--- lib/order.rb
+++ lib/order.rb
@@ -1,15 +1,19 @@
+require 'csv'
+require_relative 'customer'
+
 class Order
   attr_reader :id # WELP, HAD TO MAKE ASSUMPTIONS FOR THE REST...
   attr_accessor :products, :customer, :fulfillment_status
 
-  def initialize(id, products, customer, fulfillment_status=:pending)
+  # NOW FOR WAVE 2, WHEN I UNCOMMENT THIS, I GET AN ERROR UNDEFINED FULFILLMENT STATUS LOCAL VARIABLE???
+  # acceptable_fulfillment_status = [:pending, :paid, :processing, :shipped, :complete]
+  # raise ArgumentError.new("status must be pending, paid, processing, shipped or complete") unless acceptable_fulfillment_status.include?(fulfillment_status)
+
+  def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
-    @products = products # products collection data
+    @products = products # products collection data hash
     @customer = customer
     @fulfillment_status = fulfillment_status
-
-    acceptable_fulfillment_status = [:pending, :paid, :processing, :shipped, :complete]
-    raise ArgumentError.new("status must be pending, paid, processing, shipped or complete") unless acceptable_fulfillment_status.include?(fulfillment_status)
   end
 
   def total
@@ -26,4 +30,24 @@ class Order
     raise ArgumentError.new("Product already exists") if @products.has_key?(name)
     @products[name] = price
   end
+
+  def self.all
+    order_array = []
+
+    CSV.read('data/orders.csv').each do |row_entry|
+    products_hash = {}
+    row_entry[1].split(';') do |product|
+          products_hash[product.split(':')[0]] = [product.split(':')[1]] # why can't I make the price .to_f????
+    end
+
+    new_order = Order.new(row_entry[0].to_i, products_hash, Customer.find((row_entry[2]).to_i), row_entry[3].to_sym)
+    order_array << new_order
+    end
+
+    return order_array
+  end
 end
+
+customer = Customer.all
+order = Order.all
+pp order
