5ea80d73bf796d76635f2965ac0c736a351d308a
Tram Bui <pbui17@cmc.edu>
2020-09-23 21:21:10 -0500

    finished wave 2 for customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -6,6 +8,30 @@ class Customer
     @id = id
     @email = email
     @address = address
+  end
+
+  # self.all and self.find are class methods
+  # return a collection of Customer instances, representing all of the Customers describe in the CSV file
+  # dp we need params for the all class method?
+  def self.all
+    # need to read in the csv
+    # how do I transfer CSV data into an array?
+    customer_array = []
+    customer_data = CSV.read('data/customers.csv').map { |customer| customer.to_a }
+    customer_data.each do |customer|
+      id = customer[0].to_i
+      email = customer[1]
+      #delivery_address is a hash
+      address = {street: customer[2], city: customer[3], state: customer[4] , zip: customer[5]  }
+      customer_array << Customer.new(id, email, address)
+    end
+    return customer_array # => array of all the customer instances
+  end
+
+  def self.find(id)
+    # should invoke Customer.all and search through the results for a customer with a matching ID
+    return all.find {|customer| customer.id == id} # returns nil if ID is not found
+    
   end
 
 end
--- lib/order.rb
+++ lib/order.rb
@@ -7,18 +7,20 @@ class Order
     @id = id
     @products = products
     @customer = customer
-    @fulfillment_status = fulfillment_status
+    @fulfillment_status = validate_fulfillment_status(fulfillment_status)
+  end
 
-    # raise Argument error if fulfillment status is not the valid symbols
-    until %i[pending paid processing shipped complete].include?(@fulfillment_status)
-      raise ArgumentError.new("invalid fullfillment status")
+  def validate_fulfillment_status(status)
+    until %i[pending paid processing shipped complete].include?(status)
+      raise ArgumentError.new("invalid fullfillment status, #{status}")
     end
+    return status
   end
 
   def total
     return 0 if @products.empty?
 
-    products_total = @products.map { |product, cost| cost }.sum
+    products_total = @products.sum { |product, cost| cost }
     final_total = (products_total * 1.075).round(2)
     return final_total
   end
@@ -30,10 +32,8 @@ class Order
 
   #optional remove_product method
   def remove_product(name)
-    raise ArgumentError.new("No product with that name was found") if @products.has_key?(name) == false
-    @products.delete(name)
-    return @products
-
+    raise ArgumentError.new("No product with that name was found") unless @products.has_key?(name)
+    return @products.delete(name)
   end
 
 
