e3bade7b1de5ce014564c5c643f5886d1a0fbd42
Jasmine <jasylop@gmail.com>
2020-09-24 13:41:28 -0700

    added self.all and self.find methods

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
 
   attr_reader :id
@@ -7,5 +9,28 @@ class Customer
     @id = id
     @email = email
     @address = address
+  end
+
+  # returns a collection of Customer instances
+  def self.all
+    customer_instances = []
+    customers = CSV.read('data/customers.csv').map { |row| row.to_a }
+    customers.each do |customer|
+      id = customer[0].to_i
+      email = customer[1]
+      address_hash = {
+        street: customer[2],
+        city: customer[3],
+        state: customer[4],
+        zip: customer[5]
+      }
+      customer_instances << Customer.new(id, email, address_hash)
+    end
+      return customer_instances
+  end
+
+  # return instance of customer (the object) or nil
+  def self.find(id)
+    Customer.all.find {|customer| customer.id == id}
   end
 end
