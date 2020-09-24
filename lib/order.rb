b08dca1f708e72e1126dff5e346657d41ff55414
Stacy <lundquist.stacy@gmail.com>
2020-09-24 08:47:14 -0700

    fixed readers, add product method, argument errors, and fulfillment status to pass tests in order class.

--- lib/order.rb
+++ lib/order.rb
@@ -1,25 +1,29 @@
-require_relative  './customer'
+require_relative './customer'
 
 class Order
-  attr_reader :ID
+  attr_reader :id, :products, :customer, :fulfillment_status
 
   def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
+    valid_statuses = [:paid, :processing, :pending, :shipped, :complete]
+    unless valid_statuses.include?(fulfillment_status)
+      raise ArgumentError, "Not a valid fulfillment status."
+    end
   end
 
   def total
     sum_with_tax = (@products.values.sum) * 1.075
-     return sum_with_tax.round(2)
+    return sum_with_tax.round(2)
   end
 
   def add_product(name, price)
-    if @products.any? (name)
+    if @products.key?(name)
       raise ArgumentError, 'That product already exists.'
+    else
+      return @products[name] = price
     end
-    new_product[name] = price
-    product << new_product
   end
 end
--- test/order_test.rb
+++ test/order_test.rb
@@ -18,8 +18,8 @@ describe "Order Wave 1" do
     Customer.new(123, "a@a.co", address)
   end
 
-  xdescribe "#initialize" do
-    xit "Takes an ID, collection of products, customer, and fulfillment_status" do
+  describe "#initialize" do
+    it "Takes an ID, collection of products, customer, and fulfillment_status" do
       id = 1337
       fulfillment_status = :shipped
       order = Order.new(id, {}, customer, fulfillment_status)
@@ -37,7 +37,7 @@ describe "Order Wave 1" do
       expect(order.fulfillment_status).must_equal fulfillment_status
     end
 
-    xit "Accepts all legal statuses" do
+    it "Accepts all legal statuses" do
       valid_statuses = %i[pending paid processing shipped complete]
 
       valid_statuses.each do |fulfillment_status|
@@ -46,12 +46,12 @@ describe "Order Wave 1" do
       end
     end
 
-    xit "Uses pending if no fulfillment_status is supplied" do
+    it "Uses pending if no fulfillment_status is supplied" do
       order = Order.new(1, {}, customer)
       expect(order.fulfillment_status).must_equal :pending
     end
 
-    xit "Raises an ArgumentError for bogus statuses" do
+    it "Raises an ArgumentError for bogus statuses" do
       bogus_statuses = [3, :bogus, 'pending', nil]
       bogus_statuses.each do |fulfillment_status|
         expect {
@@ -78,8 +78,8 @@ describe "Order Wave 1" do
     end
   end
 
-  xdescribe "#add_product" do
-    xit "Increases the number of products" do
+  describe "#add_product" do
+    it "Increases the number of products" do
       products = { "banana" => 1.99, "cracker" => 3.00 }
       before_count = products.count
       order = Order.new(1337, products, customer)
@@ -89,7 +89,7 @@ describe "Order Wave 1" do
       expect(order.products.count).must_equal expected_count
     end
 
-    xit "Is added to the collection of products" do
+    it "Is added to the collection of products" do
       products = { "banana" => 1.99, "cracker" => 3.00 }
       order = Order.new(1337, products, customer)
 
@@ -97,7 +97,7 @@ describe "Order Wave 1" do
       expect(order.products.include?("sandwich")).must_equal true
     end
 
-    xit "Raises an ArgumentError if the product is already present" do
+    it "Raises an ArgumentError if the product is already present" do
       products = { "banana" => 1.99, "cracker" => 3.00 }
 
       order = Order.new(1337, products, customer)
