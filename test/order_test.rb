a6cf029fd9a8c44d1304d72c32b5f9e007b7873b
Ren Carothers <rencarothers@gmail.com>
2020-09-24 10:41:01 -0700

    wrote test for Returns accurate information about the last order

--- test/order_test.rb
+++ test/order_test.rb
@@ -118,6 +118,17 @@ describe "Order Wave 2" do
   describe "Order.all" do
     it "Returns an array of all orders" do
       # TODO: Your test code here!
+            orders = Order.all
+
+            expect(orders.length).must_equal 100
+            orders.each do |c|
+              expect(c).must_be_kind_of Order
+
+              expect(c.id).must_be_kind_of Integer
+              expect(c.products).must_be_kind_of Hash
+              expect(c.customer).must_be_kind_of Customer
+              expect(c.fulfillment_status).must_be_kind_of Symbol
+            end
     end
 
     it "Returns accurate information about the first order" do
@@ -142,6 +153,23 @@ describe "Order Wave 2" do
 
     it "Returns accurate information about the last order" do
       # TODO: Your test code here!
+      id = 100
+      products = {
+          "Amaranth" => 83.81,
+          "Smoked Trout" => 70.6,
+          "Cheddar" => 5.63
+      }
+      customer_id = 20
+      fulfillment_status = :pending
+
+      order = Order.all.last
+
+      # Check that all data was loaded as expected
+      expect(order.id).must_equal id
+      expect(order.products).must_equal products
+      expect(order.customer).must_be_kind_of Customer
+      expect(order.customer.id).must_equal customer_id
+      expect(order.fulfillment_status).must_equal fulfillment_status
     end
   end
 
