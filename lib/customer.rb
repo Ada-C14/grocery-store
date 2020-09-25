811477ebb646f7168ffd2c3e4dd59c47ee2dc23c
Yingjing Lin <yjlin789@gmail.com>
2020-09-24 23:04:19 -0700

    create class Customer and Order

---
+++ lib/customer.rb
@@ -0,0 +1,13 @@
+class Customer
+  attr_reader :id
+  attr_accessor :email, :address
+
+  def initialize
+    @id
+    @email
+    @address
+  end
+
+
+
+end
---
+++ lib/order.rb
@@ -0,0 +1,31 @@
+class Order
+  attr_reader :id
+
+  TAX_RATE = 0.075
+
+  def initialize(id, products, customer)
+    @id
+    @products
+    @customer
+    @status = :pending
+  end
+
+  def total
+    return (@products.values.sum * (1 + TAX_RATE)).round(2)
+  end
+
+  def add_product(product_name, price)
+    if @products.keys.inclued? product_name
+      raise ArgumentError
+    else
+      @products << {product_name => price}
+    end
+    return @products
+  end
+
+  def remove_product(product_name)
+
+    @products.delete(product_name)
+    return @products
+  end
+end
