d30920ff5fc51cbf73c0be9adb509c9513208850
Hanh Solo <minhhanhnyc@gmail.com>
2020-09-24 20:32:50 -0700

    Create an instructor with parameters and 2 helper methods for reading and writing the parameters

---
+++ lib/customer.rb
@@ -0,0 +1,13 @@
+class Customer
+  attr_reader :id
+  attr_accessor :email, :address
+
+  # Constructor
+  def initialize(id, email, address)
+    @id = id
+    @email = email
+    @address = address
+  end
+
+
+end
