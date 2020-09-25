e003987917bf69ea8d561d4c1fe8223c21684b11
Stacy <lundquist.stacy@gmail.com>
2020-09-24 17:44:12 -0700

    Added headers to customers csv, required it in customer class for self.all method.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,11 @@
+require 'csv'
+
+headers = ['id','email','address']
+
+CSV.open('customers.csv', 'a+', {force_quotes: true}) do |csv|
+  csv << headers if csv.count.eql? 0
+end
+
 
 class Customer
   attr_accessor :email, :address
@@ -9,11 +17,13 @@ class Customer
     @address = address
   end
 
-  def summary
-    return "Customer #{@id} is at #{@address}. The email is #{@email}"
+  def self.all
+    return CSV.read('../data/customers.csv', headers: true).map {|row| row.to_a}
   end
+  #
+  # def self.find(id)
+  #
+  #
+  # end
 end
 
-# stacy = Customer.new()
-#
-# stacy.id
