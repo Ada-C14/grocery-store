a598476321f51e90deb0b3735ea7cca72b5b5781
Stacy <lundquist.stacy@gmail.com>
2020-09-24 21:58:43 -0700

    Completed wave 2 order tests for order.find. All passing.

--- test/order_test.rb
+++ test/order_test.rb
@@ -189,17 +189,23 @@ describe "Order Wave 2" do
     end
   end
 
-  xdescribe "Order.find" do
-    xit "Can find the first order from the CSV" do
-      # TODO: Your test code here!
+  describe "Order.find" do
+    it "Can find the first order from the CSV" do
+      first = Order.find(1)
+
+      expect(first).must_be_kind_of Order
+      expect(first.id).must_equal 1
     end
 
-    xit "Can find the last order from the CSV" do
-      # TODO: Your test code here!
+    it "Can find the last order from the CSV" do
+      last = Order.find(100)
+
+      expect(last).must_be_kind_of Order
+      expect(last.id).must_equal 100
     end
 
-    xit "Returns nil for an order that doesn't exist" do
-      # TODO: Your test code here!
+    it "Returns nil for an order that doesn't exist" do
+      expect(Order.find(53145)).must_be_nil
     end
   end
 end
