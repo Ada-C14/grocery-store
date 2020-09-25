dcb1df563778f8e08e49588428190607ca2c23f0
Lina Do <l.do5147@gmail.com>
2020-09-25 16:16:34 -0700

    added a customer.find method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,6 +1,4 @@
-require 'customer.csv'
-
-
+require 'csv'
 
 class Customer
   attr_reader :id, :email, :address
@@ -10,4 +8,28 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    customers = CSV.read('data/customers.csv', headers: true).map do |row|
+      row.to_h
+    end
+    
+    return customers.map do |customer|
+      address = {
+          street: customer["street"],
+          city: customer["city"],
+          state: customer["state"],
+          zip: customer["zip"]
+      }
+
+      Customer.new(customer["id"].to_i, customer["email"], address)
+    end
+  end
+
+  def self.find(id)
+    return self.all.find { |customer| customer.id == id }
+  end
+
 end
+
+
