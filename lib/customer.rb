62f7d5ef4655dc55d86d5523d4e054b4a4e54f62
Sophie Messing <sophie.e.messing@gmail.com>
2020-09-25 07:13:45 -0700

    all wave 2 customer tests passing

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
 
   attr_reader :id
@@ -7,6 +9,38 @@ class Customer
     @id = id
     @email = email
     @address = address
+  end
+
+  # I have a scope issue here below.
+
+  def self.all
+
+    all_customers = []
+
+    customer_array = CSV.read('data/customers.csv')
+    customer_array.each do |customer|
+
+      address = {}
+
+      id = customer[0].to_i
+      email = customer[1]
+      address[:street] = customer[2]
+      address[:city] = customer[3]
+      address[:state] = customer[4]
+      address[:zip] = customer[5]
+
+      all_customers << Customer.new(id, email, address)
+
+    end
+    return all_customers
+  end
+
+  p self.all.first
+  p self.all.last
+
+  def self.find(id)
+    customer = self.all.find { |customer| customer.id == id }
+    return customer
   end
 
 end
