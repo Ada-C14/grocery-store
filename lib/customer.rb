8a4d5df82c1e7c78aa756f906ef4969cd9fc5d48
Ringo Alcock <ringo.alcock@gmail.com>
2020-09-25 15:17:34 -0700

    define self.all method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,7 @@
+require "CSV"
+
+CUSTOMERS = CSV.read("data/customers.csv").map { |row| row.to_a }
+
 class Customer
 
   def initialize(id, email, address)
@@ -9,5 +13,21 @@ class Customer
   attr_reader :id
 
   attr_accessor :email, :address
+
+  def self.all
+    all_customers = []
+    CUSTOMERS.each do |customer|
+      new_id = customer[0].to_i
+      new_email = customer[1]
+      new_address = {
+          street: customer[2],
+          city: customer[3],
+          state: customer[4],
+          zip: customer[5]
+      }
+      all_customers << Customer.new(new_id, new_email, new_address)
+    end
+    return all_customers
+  end
 
 end
