b4b5a89766be74c364e4686660cd62d665f6209f
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-09-24 21:52:56 -0700

    add tests for wave2

--- test/order_test.rb
+++ test/order_test.rb
@@ -153,6 +153,17 @@ describe "Order Wave 2" do
   describe "Order.all" do
     it "Returns an array of all orders" do
       # TODO: Your test code here!
+      orders = Order.all
+
+      expect(orders.length).must_equal 100
+      orders.each do |order|
+        expect(order).must_be_kind_of Order
+        expect(order.id).must_be_kind_of Integer
+        expect(order.products).must_be_kind_of Hash
+        expect(order.customer).must_be_kind_of Customer
+        expect(order.customer.id).must_be_kind_of Integer
+        expect(order.fulfillment_status).must_be_kind_of Symbol
+      end
     end
 
     it "Returns accurate information about the first order" do
@@ -177,20 +188,36 @@ describe "Order Wave 2" do
 
     it "Returns accurate information about the last order" do
       # TODO: Your test code here!
+      last = Order.all.last
+
+      expect(last.id).must_equal 100
+      expect(last.products).must_equal Hash("Amaranth"=> 83.81, "Smoked Trout"=> 70.6, "Cheddar"=> 5.63)
+      expect(last.customer).must_be_kind_of Customer
+      expect(last.customer.id).must_equal 20
+      expect(last.fulfillment_status).must_equal :pending
     end
   end
 
   describe "Order.find" do
     it "Can find the first order from the CSV" do
       # TODO: Your test code here!
+      first = Order.find(1)
+
+      expect(first).must_be_kind_of Order
+      expect(first.id).must_equal 1
     end
 
     it "Can find the last order from the CSV" do
       # TODO: Your test code here!
+      last = Order.find(100)
+
+      expect(last).must_be_kind_of Order
+      expect(last.id).must_equal 100
     end
 
     it "Returns nil for an order that doesn't exist" do
       # TODO: Your test code here!
+      expect(Order.find(53145)).must_be_nil
     end
   end
 end
