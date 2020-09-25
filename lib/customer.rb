f3477580e786bc7865c69ab5210b4c1f5cde8719
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-09-24 21:32:37 -0700

    CSV can be read, but the the values are not currently set up properly

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
 
   attr_reader :id
@@ -14,5 +16,46 @@ class Customer
   end
 end
 
+def self.all(file)
+  customers = []
+  CSV.read(file).each do |record|
+    customer = {
+        "ID" => record[1],
+        "Name" => record[1],
+        "Address 1" => record[2],
+        "City" => record[3],
+        "State" => record[4],
+        "Zip Code" => record[5]
+    }
+    customers << customer
+  end
+end
+
+pp self.all('../data/customers.csv')
+
+# def self.find(id)
+#
+# end
+
+
+
+# CSV.read()
 # kayla = Customer.new(446456, "kayla@uw.edu", {1 => 2})
 # pp kayla
+#
+#   CSV.read(filename, headers: true).each do |record|
+#     medalist = {
+#         "ID" => record["ID"],
+#         "Name" => record["Name"],
+#         "Height" => record["Height"],
+#         "Team" => record["Team"],
+#         "Year" => record["Year"],
+#         "City" => record["City"],
+#         "Sport" => record["Sport"],
+#         "Event" => record["Event"],
+#         "Medal" => record["Medal"]
+#     }
+#     olympic_medalists << medalist
+#   end
+#
+#   return olympic_medalists
