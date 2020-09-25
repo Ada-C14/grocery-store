01727b82dff36c3bb87b6d1073d6923b02f0bfa4
Lina Do <l.do5147@gmail.com>
2020-09-24 20:10:32 -0700

    included the total method in the order class

--- lib/order.rb
+++ lib/order.rb
@@ -6,14 +6,25 @@ class Order
     @id = id
     @products = products
     @customer = customer
-    @fulfillment_status = fulfillment_status
-    if %w[pending paid processing shipped complete].include? (fulfillment_status)
+
+    # compared the fulfillment status against the acceptable statuses
+    if %i[pending paid processing shipped complete].include? (fulfillment_status)
       @fulfillment_status = fulfillment_status
     else
       raise ArgumentError.new("#{fulfillment_status} is an invalid fulfillment status")
     end
   end
 
+  # create a total method which will calculate the total cost of the order
+  def total
+    # sum up the products
+    # products come in as a hash, item/ price
+    cost = products.values.sum
+    # add 7.5% tax
+    total_cost = (cost * 1.075).round(2)
+    # round to two decimal places
+    return total_cost
+  end
 
 end
 
