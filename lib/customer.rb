07ea845d5be4a544bc5b664e1c1773eda25d6744
Anna Kim <anna.kim93@gmail.com>
2020-09-25 15:55:16 -0400

    added class method .all

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -7,4 +9,24 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    all_customers = CSV.read('data/customers.csv').map { |row| row.to_a }
+
+    all_customers = all_customers.map do |customer|
+      id = customer[0]
+      email = customer[1]
+      address = {
+          street: customer[2],
+          city: customer[3],
+          state: customer[4],
+          zip: customer[5]
+      }
+      Customer.new(id, email, address)
+    end
+
+    return all_customers
+  end
+
+
 end
