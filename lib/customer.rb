39114f82284773ec11442654f98e1ffa90626002
KayKay-git <kayalew@yahoo.com>
2020-09-26 16:39:00 -0400

    Wave 2 Complete -Cusomer

--- lib/customer.rb
+++ lib/customer.rb
@@ -11,4 +11,32 @@ class Customer
     @address = address
   end
 
+  def self.all
+
+    customers_csv = CSV.read('data/customers.csv').map { |row| row.to_a }
+
+    customer_instances = []
+
+    customers_csv.each do |customer|
+      id = customer[0].to_i
+      email = customer[1]
+      address = {
+          street: customer[2],
+          city: customer[3],
+          state: customer[4],
+          zip: customer[5]
+      }
+
+      customer_instances << Customer.new(id, email, address)
+    end
+    return customer_instances
+  end
+
+  #returns an instance of Customer where the value of the id field in the CSV matches the passed parameter
+  def self.find(id)
+    all.each do |customer|
+      return customer if customer.id == id
+    end
+    return nil
+  end
 end
