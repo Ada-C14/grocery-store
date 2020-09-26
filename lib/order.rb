9bee8efef7e28fb533edba65440c4274e8787d8e
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-25 19:53:57 -0700

    add remove_product method in order.rb

--- lib/order.rb
+++ lib/order.rb
@@ -29,5 +29,13 @@ class Order
       @products[product_name] = price
     end
   end
+
+  def remove_product(product_name)
+    if @products.has_key?(product_name)
+      @products.delete(product_name)
+    else
+      raise ArgumentError.new("That product does not exist.")
+    end
+  end
 end
 
