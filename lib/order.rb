dec6121e37932ea2f63d01992525dbdf46469e4f
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-25 19:07:32 -0700

    fix typos in  for order.rb

--- lib/order.rb
+++ lib/order.rb
@@ -1,16 +1,17 @@
 VALID_STATUS = [:pending, :paid, :processing, :shipped, :complete]
+TAX = 0.075
 
 class Order
-  attr_reader :id, :customer, :fulfilment_status
+  attr_reader :id, :customer, :fulfillment_status
   attr_accessor :products
 
-  def initialize(id, products, customer, fulfilment_status = :pending)
-    raise ArgumentError.new("Not a valid status.") if !VALID_STATUS.include?(fulfilment_status)
+  def initialize(id, products, customer, fulfillment_status = :pending)
+    raise ArgumentError.new("Not a valid status.") if !VALID_STATUS.include?(fulfillment_status)
 
     @id = id
     @products = products
     @customer = customer
-    @fulfilment_status = fulfilment_status
+    @fulfillment_status = fulfillment_status
   end
 end
 
