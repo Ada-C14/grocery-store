7b3fe10f190827e5e6513496ae743978ecea6dc9
KayKay-git <kayalew@yahoo.com>
2020-09-26 16:45:41 -0400

    Wave 2 Complete - Order

--- lib/order.rb
+++ lib/order.rb
@@ -37,4 +37,32 @@ class Order
       @products[product_name] = price
     end
   end
+
+  def self.all
+    # returns order instances, representing all the orders represented in the CSV file
+    orders_csv = CSV.read('data/orders.csv').map { |row| row.to_a }
+
+    order_instances = []
+
+    orders_csv.each do |order|
+
+      order_id = order[0].to_i
+
+      order_hsh = {}
+      product_items_arr = order[1].split(';')
+      product_items_arr.each do |item|
+        product = item.split(':')
+        product_key = product[0]
+        product_value = product[1]
+        order_hsh[product_key.to_s] = product_value.to_f
+      end
+
+      customer_id = Customer.find(order[2].to_i)
+      status = order[3].to_sym
+
+      order_instances << Order.new(order_id, order_hsh, customer_id, status)
+
+    end
+    return order_instances
+  end
 end
