920b1f348d16680fdfa44af171a78352842689f5
r-spiel <richelle.mehlhaff@gmail.com>
2020-09-26 16:35:57 -0700

    add options wave 3, write to csv as a class method in customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -37,7 +37,23 @@ class Customer
     return found_customer
   end
 
+  def self.save(filename, new_customer)
+    #filename: a string, which is a relative path to a new CSV file to create
+    # new_customer, an instance of Customer, which is a new customer
+    CSV.open("#{filename}", "a") do |csv|
+      customer_data = [new_customer.id, new_customer.email, new_customer.address]
+      csv << customer_data
+    end
+
+    return true
+  end
 
 end
 
 #p Customer.all
+#
+# jared = Customer.new(777, "j@ada.ord", {:street=>"123 Main", :city=>"Seattle", :state=>"WA", :zip=>"98101"})
+#
+# Customer.save('../data/new_customer_list.csv', jared)
+
+
