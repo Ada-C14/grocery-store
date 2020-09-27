19836a3824b5c71235b68597560c229331a3423f
Schanen Ryan <schanen.r@gmail.com>
2020-09-26 19:04:23 -0700

    wave 2 order.all method completed and tests written and passing

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,5 @@
+require 'customer.rb'
+require 'csv'
 
 class Order
 
@@ -35,6 +37,42 @@ class Order
       raise ArgumentError, "This product is not in inventory."
     else @products.reject! {|key| key == product_name}
     end
+  end
+
+  def self.all
+    cust_order = CSV.read('./data/orders.csv', headers: false).map do |row|
+      id = row[0].to_i
+
+      products = self.product_to_hash(row[1])
+
+      customer = Customer.find(row[2].to_i)
+
+      fulfillment_status = row[3] == nil ? nil : row[3].to_sym
+
+      Order.new(id, products, customer, fulfillment_status)
+    end
+    return cust_order
+  end
+
+  def self.find(id)
+    return self.all.find {|customer| customer.id == id }
+  end
+
+  private
+
+  def self.product_to_hash(order_string)
+
+    products = {}
+
+    single_products = order_string.split(';')
+
+    single_products.each do |product|
+      hash_pair = product.split(':')
+      products[hash_pair[0]] = hash_pair[1].to_f
+    end
+
+    return products
+
   end
 
 end
--- test/order_test.rb
+++ test/order_test.rb
@@ -147,10 +147,19 @@ describe "Order Wave 1" do
 end
 
 # TODO: change 'xdescribe' to 'describe' to run these tests
-xdescribe "Order Wave 2" do
+describe "Order Wave 2" do
   describe "Order.all" do
     it "Returns an array of all orders" do
-      # TODO: Your test code here!
+      orders = Order.all
+
+      expect(orders.length).must_equal 100
+      orders.each do |o|
+        expect(o).must_be_kind_of Order
+
+        expect(o.id).must_be_kind_of Integer
+        expect(o.products).must_be_kind_of Hash
+        expect(o.customer).must_be_kind_of Customer
+      end
     end
 
     it "Returns accurate information about the first order" do
@@ -162,7 +171,6 @@ xdescribe "Order Wave 2" do
       }
       customer_id = 25
       fulfillment_status = :complete
-
       order = Order.all.first
 
       # Check that all data was loaded as expected
@@ -174,11 +182,25 @@ xdescribe "Order Wave 2" do
     end
 
     it "Returns accurate information about the last order" do
-      # TODO: Your test code here!
+      id = 100
+      products = {
+          "Amaranth" => 83.81,
+          "Smoked Trout" => 70.6,
+          "Cheddar" => 5.63
+      }
+      customer_id = 20
+      fulfillment_status = :pending
+      order = Order.all.last
+
+      expect(order.id).must_equal id
+      expect(order.products).must_equal products
+      expect(order.customer).must_be_kind_of Customer
+      expect(order.customer.id).must_equal customer_id
+      expect(order.fulfillment_status).must_equal fulfillment_status
     end
   end
 
-  describe "Order.find" do
+  xdescribe "Order.find" do
     it "Can find the first order from the CSV" do
       # TODO: Your test code here!
     end
