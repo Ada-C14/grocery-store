0c55b644a9b50f7af610e1d70191d1785db9c794
Mackenzie <mvlofthus@gmail.com>
2020-09-27 14:46:03 -0700

    with written tests added

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
   def initialize (id, email, address)
     @id = id #number
@@ -8,4 +10,34 @@ class Customer
   attr_reader :id
   attr_accessor :email, :address
 
+  def self.all
+    customers_temp = CSV.read('data/customers.csv').map do |row|
+      { "id" => row[0].to_i,
+        "email" => row[1],
+        "address" => {
+            :street => row[2],
+            :city => row[3],
+            :state => row[4],
+            :zip => row[5]
+        }
+      }
+    end
+
+    customers = []
+    customers_temp.each do |row|
+      customers << Customer.new(row["id"], row["email"], row["address"])
+    end
+
+    return customers
+  end
+
+  def self.find(id)
+    found = Customer.all.select { |customer| customer.id == id }
+
+    return found[0]
+  end
+
+
 end
+
+print Customer.find(3)
--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,20 @@
+require_relative 'customer.rb'
+require 'csv'
+
+def product_format(string)
+  formatted = Hash.new(0)
+
+  products_split = string.split(';')
+
+  products_split.each do |product|
+    cost_split = product.split(':')
+    formatted[cost_split[0]] = cost_split[1].to_f
+  end
+
+  return formatted
+end
+
+
 class Order
   def initialize (id, products, customer, fulfillment_status = :pending)
     @id = id #number
@@ -5,24 +22,20 @@ class Order
     @customer = customer #string?
     @fulfillment_status = fulfillment_status
       status_options = [:pending, :paid, :processing, :shipped, :complete]
-      if status_options.include? fulfillment_status
-        @fulfillment_status = fulfillment_status
-      else
-        raise ArgumentError
-      end #symbol with qualifications, should method be here?
+      raise ArgumentError.new("Not a valid fulfillment status.") if status_options.include?(fulfillment_status) == false
   end
 
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
 
-  def status_update (fulfillment_status)
-    status_options = [:pending, :paid, :processing, :shipped, :complete]
-    if status_options.include? @fulfillment_status
-      @fulfillment_status = fulfillment_status
-    else
-      raise ArgumentError
-    end
-  end
+  # def status_update (fulfillment_status)
+  #   status_options = [:pending, :paid, :processing, :shipped, :complete]
+  #   if status_options.include? @fulfillment_status
+  #     @fulfillment_status = fulfillment_status
+  #   else
+  #     raise ArgumentError
+  #   end
+  # end
 
 
 
@@ -33,7 +46,6 @@ class Order
     end
 
     sum *= 1.075
-
     total = '%.2f' % sum
 
     return  total.to_f
@@ -44,7 +56,31 @@ class Order
       raise ArgumentError
     else @products[name] = price
     end
+  end
 
+  def self.all
+    orders_temp = CSV.read('data/orders.csv').map do |row|
+      { "id" => row[0].to_i,
+        "products" => product_format(row[1]),
+        "customer_id" => row[2].to_i, #turn the Customer ID into an instance of Customer
+        "status" => row[3].to_sym
+      }
+    end
+
+    orders = []
+    orders_temp.each do |row|
+      orders << Order.new(row["id"], row["products"], Customer.find(row["customer_id"]), row["status"])
+    end
+
+    return orders
+  end
+
+
+
+  def self.find(id)
+    found = Order.all.select { |order| order.id == id }
+
+    return found[0]
   end
 
 end
