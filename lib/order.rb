31a2b52685a5e9ead917654069edb8baa58c5534
Alice D <alicedilanchian@gmail.com>
2020-09-27 15:24:14 -0500

    Wave 2 Completed

--- lib/order.rb
+++ lib/order.rb
@@ -1,51 +1,80 @@
 require_relative "customer"
+require "csv"
 
 class Order
-  attr_reader :id_number
+  attr_reader :id, :products, :customer, :fulfillment_status
 
-  def initialize(id_number, h_product_price, customer, fulfillment_status = "complete")
-    @id_number = id_number
-    @h_product_price = h_product_price
+  def initialize(id, products, customer, fulfillment_status = :pending)
+    @id = id
+    @products = products
     @customer = customer
+
     @fulfillment_status = fulfillment_status
-  end
-  def status
-    @options = ["pending", "paid", "processing", "shipped", "complete"]
+    @options = [:pending, :paid, :processing, :shipped, :complete]
     if @options.include?(@fulfillment_status) == false
       raise ArgumentError
-    else
-      return @fulfillment_status
     end
+
   end
+
   def total
     @total_cost = 0.00
-    if @h_product_price == nil
+    if @products == nil
       return @total_cost = 0.0
     else
-    @h_product_price.each_value { |value| @total_cost += value }
-    @total_cost += (0.075*@total_cost).round(2)
+      @products.each_value { |value| @total_cost += value }
+      @total_cost += (0.075 * @total_cost).round(2)
     end
   end
 
   def add_product(product_name, price)
-    @product_name = product_name.to_sym
+    @product_name = product_name
     @price = price
-    puts @product_name
-    if @h_product_price.key?(@product_name)
+    if @products.key?(@product_name)
       raise ArgumentError
     else
-      @h_product_price.store(@product_name, @price)
+      @products.store(@product_name, @price)
     end
   end
 
-end
-hash1_address = {:street_number=> "14", :street=> "Park Road", :city=> "London", :zip_code=> "91202"}
-customer1 = Customer.new("101020", "oliver.twist@gmail.com", hash1_address )
-hash1 = {:butter => 3.50, :flour => 4.00, :milk => 2.00}
-hash2 = {}
-order1 = Order.new("010203", hash1, customer1, "paid")
-total1 = order1.total
-puts total1
-puts order1.status
+  def self.all
+    # everything from the csv file is going in here as strings as of right now; each row is a an array
+    # an array of arrays (rows) of strings(each column)- separated by commas
+    @arr_of_instances = CSV.read('C:\Users\alice\ADA\grocery-store\data\orders.csv').map do |row|
+      # ================>>>>>>> 1,Lobster:17.18;Annatto seed:58.38;Camomile:83.21,25,complete
+      hash_products_perorder = {}
+      array1 = row[1].split(';')
+      # array1 = [Lobster:17.18, Annatto seed:58.38, Camomile:83.21]
 
-order1.add_product("bread", 3.00)
+      array1.each do |element|
+        keyandvalue = element.split(':')
+        # keyvalue = [Lobster, 17.18]
+        k = keyandvalue[0]
+        # k = Lobster
+        v = keyandvalue[1]
+        # v = 17.18
+        hash_products_perorder[k] = v.to_f
+      end
+
+      customer = Customer.find(row[2].to_i)
+
+      Order.new(row[0].to_i, hash_products_perorder, customer, row[3].to_sym)
+    end
+    return @arr_of_instances
+  end
+
+  def self.find(id)
+    values = Order.all
+    values.each do |instance|
+      if instance.id == id
+        return instance
+      end
+    end
+    return nil
+  end
+end
+
+# thisthing = Order.all
+# puts thisthing[0].products
+# puts thisthing[99].customer.email
+
