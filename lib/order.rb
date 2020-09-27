a4c4bd3eed3db075c8a3a4c23e9874d334ce0a4d
Marj E <marj@marjorieetienne.com>
2020-09-27 12:59:29 -0700

    added gems for csv and awesomeprint

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,8 @@
+require 'csv'
+require 'awesome_print'
+
+#CSV.read('planets_data.csv').map { |row| row.to_a } when not using headers
+
 class Order
   attr_reader :id
   attr_accessor :products_prices
