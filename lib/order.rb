bd6d17de9c5f6420527bbd30599b071193431d52
Jessica Chan <jwinchan@gmail.com>
2020-09-28 03:36:56 -0700

    added self.parse, self.all, and self.find methods to order.rb

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,8 @@
+require 'csv'
 require 'customer'
 
 class Order
-  attr_reader :id, :customer
-  attr_accessor :products, :fulfillment_status
+  attr_reader :id, :products, :customer, :fulfillment_status
 
   def initialize(id, products, customer, fulfillment_status = :pending)
     raise ArgumentError unless [:pending, :paid, :processing, :shipped, :complete].include?(fulfillment_status)
@@ -22,7 +22,29 @@ class Order
 
   def add_product(product_name, price)
     raise ArgumentError if @products.has_key?(product_name)
-
     @products[product_name] = price
+  end
+
+  def self.parse(products)
+    product_hash = {}
+    products.split(";").each do |item|
+      details = item.split(":")
+      product_hash[details[0]] = details[1].to_f
+    end
+    return product_hash
+  end
+
+  def self.all
+    orders = CSV.read('data/orders.csv').map do |row|
+      Order.new(row[0].to_i,
+                parse(row[1]),
+                Customer.find(row[2].to_i),
+                row[3].to_sym)
+    end
+    return orders
+  end
+
+  def self.find(id)
+    return self.all.find{|order| order.id == id}
   end
 end
