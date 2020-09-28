6711fb7ce0dbd8be93882d380fb3d0d0abf67521
Marj E <marj@marjorieetienne.com>
2020-09-27 20:41:15 -0700

    added and modified solution for wave 2 of Customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,6 @@
 require 'csv'
 require 'awesome_print'
 
-#CSV.read('planets_data.csv').map { |row| row.to_a } when not using headers
 
 class Customer
   attr_reader :id
@@ -12,4 +11,22 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    modified_csv_data = CSV.read('./data/customers.csv').map do |customer|
+      Customer.new(customer[0].to_i,
+                   customer[1],
+                   Hash[:street, customer[2], :city, customer[3], :state,customer[4], :zip, customer[5]])
+    end
+
+    return modified_csv_data
+  end
+
+  def self.find(id)
+    # - `self.find(id)` - returns an instance of `Customer` where the value of the id field in the CSV matches the passed parameter
+    #  `Customer.find` should not parse the CSV file itself.
+    # Instead it should invoke `Customer.all` and search through the results for a customer with a matching ID.
+  end
 end
+
+ap Customer.all
