fa95ddcec6369891bed352dea51bec457b493bdd
Iris Lux <iris.lux0@gmail.com>
2020-09-27 20:19:23 -0700

    self.save

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,7 +14,7 @@ class Customer
   def self.all
     headers = [:id, :email, :street, :city, :state, :zip]
     customers = CSV.parse(File.read("./data/customers.csv"), headers: headers).map do |row|
-      address = self.headers_to_address(headers, row, 2, 5)
+      address = self.row_to_address(headers, row, 2, 5)
       Customer.new(row[:id].to_i, row[:email], address)
     end
     return customers
@@ -24,10 +24,18 @@ class Customer
     return self.all.find{|customer| customer.id == id}
   end
 
+  def self.save (filename, new_customer)
+    CSV.open(filename, "wb") do |csv|
+      csv << [new_customer.id, new_customer.email, new_customer.address[:street], new_customer.address[:city], new_customer.address[:state], new_customer.address[:zip]]
+    end
+  end
+
   private
-  def self.headers_to_address(headers, row,  address_begin, address_end)
+  def self.row_to_address(headers, row,  address_begin, address_end)
     address = Hash.new
     headers[address_begin..address_end].each{|header| address[header] = row[header]}
     return address
   end
+
+
 end
